7a13f3267bfbb6b63f5adf0b9a8d324d9953025e
Egon Bernardo <egonbernardo@gmail.com>
2025-04-09 00:09:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,3 +29,29 @@ document.addEventListener("DOMContentLoaded", () => {
       modal.style.display = "none";
     });
   });
+
+  let indiceAtual = 0;
+  const imagens = document.querySelectorAll('.imgcarrossel img');
+  
+  // Função para exibir a imagem atual
+  function mostrarImagem(indice) {
+      imagens.forEach(img => img.classList.remove('ativo'));
+      imagens[indice].classList.add('ativo');
+  }
+  
+  // Avança automaticamente a cada 3 segundos
+  function iniciarCarrosselAutomatico() {
+      setInterval(() => {
+          indiceAtual = (indiceAtual + 1) % imagens.length;
+          mostrarImagem(indiceAtual);
+      }, 3000);
+  }
+  
+  // Inicia o carrossel quando a página carregar
+  window.onload = () => {
+      mostrarImagem(indiceAtual);
+      iniciarCarrosselAutomatico();
+  };
+  
+
+  
